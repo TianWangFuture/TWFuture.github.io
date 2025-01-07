5fc45a3cc8242ae20caaeafe1af2c70cf19260c6
TWFuture <109783586+TianWangFuture@users.noreply.github.com>
2025-01-07 22:39:25 +0800

    Create login.js

---
+++ login.js
@@ -0,0 +1,19 @@
+function check(thisform) {
+ 
+	var name=document.getElementById("name").value;
+	if (name=="admin") {  
+		alert("admin test clear");
+	}
+
+
+	if (name=="7a853a3a-f309-4d04-9dfe-5e1f5338d379") {  
+		alert("*暗示：\n西安某學校寒假放假時間為1月y日\n其中：令f(x)=In(x)；儅x=1時，過f(x)圖像作切綫F(x)\ny的取值範圍是：儅F(x)自變量取值範圍為[17,21]時，它的因變量取值");
+	}
+
+
+
+	else{
+		alert(wlha+"\n未查询到您所填信息\n请检查填充内容是否正确 或 向我反馈\n若有疑问请输入“-c”");
+	}
+ 
+}
