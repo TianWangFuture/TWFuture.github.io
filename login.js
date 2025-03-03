40c2f48b5ea6d436efd756a6587195263689e171
TWFuture <109783586+TianWangFuture@users.noreply.github.com>
2025-03-03 23:10:14 +0800

    Update login.js

--- login.js
+++ login.js
@@ -6,14 +6,16 @@ function check(thisform) {
 	}
 
 
-	if (name=="7a853a3a-f309-4d04-9dfe-5e1f5338d379") {  
-		alert("*暗示：\n西安某學校寒假放假時間為1月y日\n其中：令f(x)=In(x)；儅x=1時，過f(x)圖像作切綫F(x)\ny的取值範圍是：儅F(x)自變量取值範圍為[17,21]時，它的因變量取值");
+	if (name=="87108") {  
+		alert("VFdGdXR1cmXnpZ3kvaDmsLjov5zlubjnpo8=");
+	}
+	if (name=="TWFuture祝你永远幸福") {  
+		alert("project TWFutureNexT completely！\n凭这张截图带时间证明解密完成");
 	}
 
 
-
 	else{
-		alert(wlha+"\n未查询到您所填信息\n请检查填充内容是否正确 或 向我反馈\n若有疑问请输入“-c”");
+		alert("\n未查询到您所填信息\n请检查填充内容是否正确 或 向我反馈\n若有疑问请输入“-c”");
 	}
  
 }
